7a0c70594958fa093fc152d27b40f9ea5959d0ce
VetrivelJaganathan <vetriveljjaganathan5125@gmail.com>
2022-07-27 17:51:10 +0530

    page linked

--- script/script.js
+++ script/script.js
@@ -1,1 +1,24 @@
-let
+let allData = [];
+
+let getDetails = (number) => {
+    document.querySelector('.texts').value = '';
+    for (let index = 1; index <= number; index++) {
+        const url = `https://rickandmortyapi.com/api/character/${index}`;
+
+        fetch(url).then((details) => {
+            details.json().then((data) => {
+                allData.push(data);
+                localStorage.setItem("person", JSON.stringify(allData));
+                display();
+            })
+        })
+    }
+}
+let display = () => {
+    let dipInfo = JSON.parse(localStorage.getItem("person"));
+    let getInfo = dipInfo.map((Name) => {
+        //console.log(Name.name);
+        return `<div>Name : ${Name.name}</div><div>Status : ${Name.status}</div>`
+    }).join('')
+    document.querySelector('.filesData').innerHTML = getInfo;
+}
